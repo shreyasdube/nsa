b3f6321114c8de9c954a62d02590c04c5c89e16f
Shreyas Dube <sdube@akamai.com>
2014-03-31 21:10:59 -0400

    added waf.getFilteredData()

--- js/waf.js
+++ js/waf.js
@@ -20,6 +20,32 @@ var waf = {
     });
   },
 
+  // returns only those data items that match the selected country and network dropdowns
+  getFilteredData: function() {
+    var country = uiUtil.getSelectedCountry();
+    var network = uiUtil.getSelectedNetwork();
+
+    console.log("filter by ", country, network);
+    return waf.data.filter(function(d) {
+      
+      // if both are *, return immediately
+      if (country === "*" && network === "*") {
+        return true;
+      } else {
+        // if country is *, then filter by network
+        if (country === "*") {
+          return d.network === network;
+        } else if (network === "*") {
+          // filter by country
+          return d.country === country;
+        } else {
+          // filter by both
+          return d.country === country && d.network === network;
+        }
+      }
+    });
+  },
+
   init: function(wafData) {
     waf.data = wafData;
   } 
