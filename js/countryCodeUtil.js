d347cbd18e671fc3ef2f56675dd1f160b0c71a63
Shreyas Dube <sdube@akamai.com>
2014-03-31 20:33:34 -0400

    added region selector

--- js/countryCodeUtil.js
+++ js/countryCodeUtil.js
@@ -1,49 +1,65 @@
 var countryCodes = {
 
-	iso2CountryCodes: {},
-	geoJsonCountryCodes: {},
+  iso2CountryCodes: {},
+  geoJsonCountryCodes: {},
 
 
-	initCountriesMap: function(countries, useIso2Code) {
-		var countriesMap = {};
-		countries.forEach(function(d) {
-			var code = useIso2Code ? d.iso2Code : d.id;
-			countriesMap[code] = d;
-		});
-		return countriesMap;
-	},
+  initCountriesMap: function(countries, useIso2Code) {
+    var countriesMap = {};
+    countries.forEach(function(d) {
+      var code = useIso2Code ? d.iso2Code : d.id;
+      countriesMap[code] = d;
+    });
+    return countriesMap;
+  },
 
-	initIsoCodeLookupTable: function(countries, wafCodes) {
-		var countriesMap = countryCodes.initCountriesMap(countries, true);
-		for (var wafCountry in wafCodes) {
-			var country = countriesMap[wafCountry];
-			countryCodes.iso2CountryCodes[wafCountry] =  {
-				geoJsonCode: country.id,
-				iso2Code: country.iso2Code,
-				name: country.name,
-				capitalCity: country.capitalCity
-			};
-		}
-	},
+  initIsoCodeLookupTable: function(countries, wafCodes) {
+    var countriesMap = countryCodes.initCountriesMap(countries, true);
+    for (var wafCountry in wafCodes) {
+      var country = countriesMap[wafCountry];
+      countryCodes.iso2CountryCodes[wafCountry] =  {
+        geoJsonCode: country.id,
+        iso2Code: country.iso2Code,
+        name: country.name,
+        capitalCity: country.capitalCity
+      };
+    }
+  },
 
-	initGeoJsonCodeLookupTable: function(countries, wafCodes) {
-		var countriesMap = countryCodes.initCountriesMap(countries, false);
-		for (var wafCountry in wafCodes) {
-			var country = countryCodes.iso2CountryCodes[wafCountry];
-			countryCodes.geoJsonCountryCodes[country.geoJsonCode] = country;
-		}
-	},
+  initGeoJsonCodeLookupTable: function(countries, wafCodes) {
+    var countriesMap = countryCodes.initCountriesMap(countries, false);
+    for (var wafCountry in wafCodes) {
+      var country = countryCodes.iso2CountryCodes[wafCountry];
+      countryCodes.geoJsonCountryCodes[country.geoJsonCode] = country;
+    }
+  },
 
-	init: function(countries, worldData, wafData) {
-		// get all waf country codes
-		var wafCodes = {};
-		wafData.forEach(function(d) {
-			var countryCode = d.country;
-			wafCodes[countryCode] = countryCode;
-		});
+  init: function(countries, worldData, wafData) {
+    // get all waf country codes
+    var wafCodes = {};
+    wafData.forEach(function(d) {
+      var countryCode = d.country;
+      wafCodes[countryCode] = countryCode;
+    });
 
-		countryCodes.initIsoCodeLookupTable(countries, wafCodes);
-		countryCodes.initGeoJsonCodeLookupTable(countries, wafCodes);
-	}
+    countryCodes.initIsoCodeLookupTable(countries, wafCodes);
+    countryCodes.initGeoJsonCodeLookupTable(countries, wafCodes);
+  }, 
+
+  getSortedCountries: function() {
+    // get all countries in sorted order
+    var countries = [];
+    for (var country in countryCodes.iso2CountryCodes) {
+      countries.push(countryCodes.iso2CountryCodes[country]);
+    }
+
+    // sort countries
+    countries.sort(function(a, b) {
+      // sort countries by name
+      return d3.ascending(a.name, b.name);
+    });
+
+    return countries;
+  }
 
 };
---
+++ js/uiUtil.js
@@ -0,0 +1,22 @@
+var uiUtil = {
+
+  initCountrySelector: function(countrySelectorId) {
+    // get all countries in sorted order
+    var countries = countryCodes.getSortedCountries();
+    console.log(countries);
+
+    // create manual entry for the entire world
+    var world = {
+      iso2Code: "*",
+      geoJsonCode: "*",
+      name: "World"
+    }
+
+    d3.select(countrySelectorId).append("select")
+        // concat world + countries
+        .selectAll("option").data([world].concat(countries))
+        .enter().append("option")
+        .attr("value", function(d) { return d.iso2Code; })
+        .text(function(d) { return d.name; });
+  }
+}
