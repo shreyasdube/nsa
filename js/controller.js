bd7d7ccd5c4be24a09721eb3b8306661a388ec01
Shreyas Dube <sdube@akamai.com>
2014-04-02 19:09:25 -0400

    moved initviz code to controller

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,25 @@
 var controller = {
+  // initialize the visualization
+  initVis: function(error, wafData, world, countries) {
+    // stash data
+    waf.init(wafData);
+    // init utils
+    countryCodes.init(countries, world);
+    uiUtil.initCountrySelector("#countrySelector");
+    uiUtil.initNetworkSelector("#networkSelector");
+    // draw world
+    drawWorld(world);
+    // overlay attacks
+    showAttacks();
+    // draw polar plot
+    drawPolar();
+  }, 
+  
   init: function() {
     queue()
       .defer(d3.csv, "../data/waf_5mi")
       .defer(d3.json, "../data/world_data.json")
       .defer(d3.json,"../data/countrycodes.json")
-      .await(initVis);
+      .await(controller.initVis);
   }
 };
